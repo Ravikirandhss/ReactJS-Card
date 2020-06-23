efbfdd3e2953f347a3310ae6e7ac3051130241ab
Ravikirandhss <dhss.ravikiran@gmail.com>
2020-06-23 15:15:57 +0530

    Updates

---
+++ precache-manifest.3999868703765561a778ea710352c22c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7205ebdc887e7d5d1e13126f37872a1d",
+    "url": "/react-card-app/index.html"
+  },
+  {
+    "revision": "4caa18bffa07869a2fdf",
+    "url": "/react-card-app/static/css/main.4d26ffb0.chunk.css"
+  },
+  {
+    "revision": "94ff1f71c41d3b5d20eb",
+    "url": "/react-card-app/static/js/2.b03fce06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-card-app/static/js/2.b03fce06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4caa18bffa07869a2fdf",
+    "url": "/react-card-app/static/js/main.3d683ce3.chunk.js"
+  },
+  {
+    "revision": "2a8a5cc7d3118e4deec9",
+    "url": "/react-card-app/static/js/runtime-main.9b8fc239.js"
+  },
+  {
+    "revision": "cb0caa49c4e5330f47af75295fe5d031",
+    "url": "/react-card-app/static/media/icon.cb0caa49.jpeg"
+  }
+]);
